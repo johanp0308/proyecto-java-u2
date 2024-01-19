ce766ae19e179fb9341f8c2f336c2770a8ac99bd
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-01-18 20:22:22 -0500

    feat: :sparkles: add something main

--- src/main/java/com/local/Main.java
+++ src/main/java/com/local/Main.java
@@ -2,6 +2,7 @@ package com.local;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Pruebas");
     }
 }
